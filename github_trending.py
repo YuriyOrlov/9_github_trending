b1f5092af1fe4210849effdd425ba202b976057f
YuriyOrlov <navuchodonsr@yandex.ru>
2017-05-02 11:27:04 +0300

    I've moved number of links, which algorithm gives back, into function as default variable and, moreover,  removed additional parameters from url.

--- github_trending.py
+++ github_trending.py
@@ -1,17 +1,15 @@
 import requests
-import json
 import datetime
 
-NUMBER_OF_REPOSITORIES_TO_SHOW = 20
 
-
-def get_trending_repositories(top_size):
+def get_trending_repositories(top_size=20):
     beginning_date = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
     todays_date = (datetime.datetime.now()).strftime('%Y-%m-%d')
-    url = 'https://api.github.com/search/repositories?q=python+created:"{}..{}"'.format(beginning_date, todays_date)
-    additional_parameters = {'sort': 'stars', 'order': 'desc', 'per_page': top_size}
+    url = 'https://api.github.com/search/repositories?'
+    additional_parameters = {'q': 'python+created:"{}..{}"'.format(beginning_date, todays_date),
+                             'sort': 'stars', 'order': 'desc', 'per_page': top_size}
     top_trending_repos = requests.get(url, params=additional_parameters)
-    return json.loads(top_trending_repos.text)
+    return top_trending_repos.json()
 
 
 def shows_needed_info_about_repositories(repos):
@@ -23,5 +21,5 @@ def shows_needed_info_about_repositories(repos):
 
 
 if __name__ == '__main__':
-    top_trending_repos = get_trending_repositories(NUMBER_OF_REPOSITORIES_TO_SHOW)
+    top_trending_repos = get_trending_repositories()
     shows_needed_info_about_repositories(top_trending_repos)
