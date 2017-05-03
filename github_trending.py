134697940ce884026c5da2b14e2640e84aef486d
YuriyOrlov <navuchodonsr@yandex.ru>
2017-05-03 10:07:05 +0300

    Added variable for how many days to use, when search repos.

--- github_trending.py
+++ github_trending.py
@@ -3,9 +3,10 @@ import datetime
 
 
 def get_trending_repositories(top_size=20):
-    beginning_date = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+    num_days_in_the_past = 7
+    beginning_date = (datetime.datetime.now() - datetime.timedelta(days=num_days_in_the_past)).strftime('%Y-%m-%d')
     todays_date = (datetime.datetime.now()).strftime('%Y-%m-%d')
-    url = 'https://api.github.com/search/repositories?'
+    url = 'https://api.github.com/search/repositories'
     additional_parameters = {'q': 'python+created:"{}..{}"'.format(beginning_date, todays_date),
                              'sort': 'stars', 'order': 'desc', 'per_page': top_size}
     top_trending_repos = requests.get(url, params=additional_parameters)
